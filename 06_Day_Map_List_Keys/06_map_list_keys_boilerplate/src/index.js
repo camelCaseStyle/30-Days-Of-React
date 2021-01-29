c84452e4c7c56e6d2a4d53a8f014a4725101f61f
Anubhav Ashish <anubhav.ashish9@gmail.com>
2021-01-30 01:02:53 +1100

    new: finish day 6 and country graph

--- 06_Day_Map_List_Keys/06_map_list_keys_boilerplate/src/index.js
+++ 06_Day_Map_List_Keys/06_map_list_keys_boilerplate/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-
-
+import {tenHighestPopulation} from './data/ten_most_highest_populations'
+const maxpop = 7693165599;
 const numbersList = ()=>{
   let numbers = []; 
   for(let i =0; i < 32; i++){
@@ -42,6 +41,45 @@ const Numbers = ({numbers})=>{
   })
   return list; 
 }
+
+
+
+const Bars = ({countries})=>{
+  const bars = countries.map(country=>{
+    let width = (country.population/maxpop)*300 + 'px';
+    return (
+      <div>
+        <div className='bar' style={{width:width, display:'inline-block'}}></div>
+      </div>
+    )
+  })
+  return bars;
+}
+
+const Countries = ({countries})=>{
+  const countryList = countries.map((country)=>{
+    return (
+      <div>
+        {country.country}
+      </div>
+    )
+  })
+  return <div>{countryList}</div>; 
+}
+
+const Population = ({countries})=>{
+  const countryList = countries.map(country=>{
+    return (
+      <div>
+        {country.population.toLocaleString()}
+      </div>
+    )
+  })
+  return countryList; 
+}
+
+
+
 // The App, or the parent or the container component
 // Functional Component
 const App = () => {
@@ -49,9 +87,20 @@ const App = () => {
     <div className='app'>
       <div>
         <h1>30 Days of React</h1>
-        <div className='all-numbers'>
-          <Numbers numbers={numbersList()}/>
-        </div>  
+        <h2>World population</h2>
+        <h3>Ten most populated countries</h3> 
+        <div className='main'>
+          <div className='countries'>
+            <Countries countries={tenHighestPopulation}/>
+          </div>
+          <div className='bars'>
+            <Bars countries={tenHighestPopulation}/>
+          </div>
+        <div className='numbers'>
+          <Population countries={tenHighestPopulation}/>
+        </div>
+        </div>
+        
       </div>
     </div>
   )
